240766faf6788c39aa10ff9bf93e29b316afb04d
noahfranco <noahfra@gmail.com>
2019-10-03 18:46:22 -0400

    added dynamic port for heroku

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const server = require("./server.js"); 
 
-server.listen(4000, () => {
-    console.log("\n *** Server is Listening on http://localhost:4000 *** \n")
+// dynamic port for heroku 
+
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+    console.log(`\n *** Server is Listening on ${port} *** \n`)
 })
 
 
